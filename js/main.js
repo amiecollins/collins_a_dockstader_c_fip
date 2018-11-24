50431e70307f07b7737072502b044241f2fd8971
amiecollins <a_collins84448@fanshaweonline.ca>
2018-11-24 17:01:45 -0500

    completed home page and added header/footer to all pages

--- js/main.js
+++ js/main.js
@@ -1,1 +1,28 @@
-// JavaScript Document
+// Slideshow effect
+
+// $("#slideshow > div:gt(0)").hide();
+
+// setInterval(functions() {
+// 	$("#slideshow > div:first")
+// 	.fadeOut(1000)
+// 	.next()
+// 	.fadeIn(1000)
+// 	.end()
+// 	.appendTo("#slideshow");
+// }, 3000);
+
+var images = ["images/pic1.jpg", "images/pic2.jpg", "images/pic3.jpg"],
+	i = 1;
+
+// load images 
+
+for (var j=images.length; j--;) {
+	var img = new Image();
+	img.src= images[j];
+}
+
+// event caller
+
+document.getElementById("slideshow").addEventListener("click", function() {
+	this.src = images[i >= images.length - 1 ? i = 0 : ++i];
+}, false);
